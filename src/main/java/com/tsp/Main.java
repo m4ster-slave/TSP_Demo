87a5eb4a50ea6b9fc92fb0a71e98c254270371b1
Lukas Weger <mail@weger.dev>
2025-01-09 08:33:23 +0100

    make jar file self contained

--- src/main/java/com/tsp/Main.java
+++ src/main/java/com/tsp/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("javafx.preloader", "none");
         Application.launch(MainView.class, args);
     }
 }
